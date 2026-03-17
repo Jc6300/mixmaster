3bb8fd4fea0d5f210b372ac316e530781059e63d
Josh <joshcotton93@gmail.com>
2026-03-17 16:36:57 -0500

    fixed issue where homepage would display incorrect logic

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -2,21 +2,21 @@ import { Outlet, useNavigate, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
-	const navigation = useNavigation();
-	const isPageLoading = navigation.state === "loading";
-	const value = "";
-	return (
-		<>
-			<Navbar />
-			<section className="page">
-				{isPageLoading ? (
-					<div className="'loading" />
-				) : (
-					<Outlet context={{ value }} />
-				)}
-			</section>
-		</>
-	);
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
+  const value = "";
+  return (
+    <>
+      <Navbar />
+      <section className="page">
+        {isPageLoading ? (
+          <div className="loading" />
+        ) : (
+          <Outlet context={{ value }} />
+        )}
+      </section>
+    </>
+  );
 };
 
 export default HomeLayout;
